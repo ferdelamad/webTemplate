d87b5faf8a8e2a8f1beecf642187a0dab618ab3e
Fernando <ferdelamad@gmail.com>
2018-08-27 15:36:36 -0700

    Styled Menu Bar

--- src/Components/CustomNavbar.jsx
+++ src/Components/CustomNavbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Menu from "./Menu";
 import "./CustomNavbar.css";
@@ -7,27 +7,29 @@ import "./CustomNavbar.css";
 export default class CustomNavBar extends Component {
   render() {
     return (
-      <Navbar default collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">NLP App</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullCenter>
-            <Menu num={1} href="/">
-              HOME
-            </Menu>
-            <Menu num={2} href="/about">
-              ABOUT
-            </Menu>
-            <Menu num={3} href="/news">
-              NEWS
-            </Menu>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div className="sticky-nav">
+        <Navbar default collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">B R A N D N A M E</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <Menu num={1} href="/">
+                H O M E
+              </Menu>
+              <Menu num={2} href="/about">
+                A B O U T
+              </Menu>
+              <Menu num={3} href="/news">
+                N E W S
+              </Menu>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,65 +9,69 @@ import Typing from "react-typing-animation";
 export default class Home extends Component {
   render() {
     return (
-      <Grid>
-        <div className="main">
-          <Jumbotron>
-            <div className="first">
-              <h2>Welcome to myWebSite.com</h2>
-              <Typing>
-                <p>
-                  This is how to build a website with React,
-                  <Typing.Backspace count={6} />
-                  React-Router and
-                  <Typing.Backspace count={16} />
-                  React-BootStrap.
-                  <Typing.Backspace count={16} />
-                </p>
-              </Typing>
-            </div>
-            <div className="second">
-              <div className="dfl">
-                <DF />
+      <div className="home">
+        <Grid>
+          <div className="main">
+            <Jumbotron>
+              <div className="first">
+                <h2>
+                  Welcome to <code>Custom WebSite</code> Template
+                </h2>
+                <Typing>
+                  <p>
+                    This is how to build a website with React,
+                    <Typing.Backspace count={6} />
+                    React-Router and
+                    <Typing.Backspace count={16} />
+                    React-BootStrap.
+                    <Typing.Backspace count={16} />
+                  </p>
+                </Typing>
               </div>
-              <div className="center">
-                <Player />
+              <div className="second">
+                <div className="dfl">
+                  <DF />
+                </div>
+                <div className="center">
+                  <Player />
+                </div>
               </div>
-            </div>
-            <Link to="/about">
-              <Button bsStyle="primary">Learn More!</Button>
-            </Link>
-          </Jumbotron>
-        </div>
-        <Row className="show-grid text-center disp">
-          <Col xs={12} sm={14} className="person-wrapper">
-            <Image
-              src="assets/cover_robot Cropped.jpg"
-              circle
-              className="profile-pic"
-            />
-            <h3>Robot</h3>
-            <p>Random Robot Text</p>
-          </Col>
-          <Col xs={12} sm={14} className="person-wrapper">
-            <Image
-              src="assets/cover_robot Cropped.jpg"
-              circle
-              className="profile-pic"
-            />
-            <h3>Robot</h3>
-            <p>Random Robot Text</p>
-          </Col>
-          <Col xs={12} sm={14} className="person-wrapper">
-            <Image
-              src="assets/cover_robot Cropped.jpg"
-              circle
-              className="profile-pic"
-            />
-            <h3>Robot</h3>
-            <p>Random Robot Text</p>
-          </Col>
-        </Row>
-      </Grid>
+              <Link to="/about">
+                <Button bsStyle="primary">Learn More!</Button>
+              </Link>
+            </Jumbotron>
+          </div>
+          <Row className="show-grid text-center disp">
+            <Col xs={12} sm={14} className="person-wrapper">
+              <Image
+                src="assets/cover_robot Cropped.jpg"
+                circle
+                className="profile-pic"
+              />
+              <h3>Robot</h3>
+              <p>Random Robot Text</p>
+            </Col>
+            <Col xs={12} sm={14} className="person-wrapper">
+              <Image
+                src="assets/cover_robot Cropped.jpg"
+                circle
+                className="profile-pic"
+              />
+              <h3>Robot</h3>
+              <p>Random Robot Text</p>
+            </Col>
+            <Col xs={12} sm={14} className="person-wrapper">
+              <Image
+                src="assets/cover_robot Cropped.jpg"
+                circle
+                className="profile-pic"
+              />
+              <h3>Robot</h3>
+              <p>Random Robot Text</p>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
